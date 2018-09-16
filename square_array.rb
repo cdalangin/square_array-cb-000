f3fe7436ab8af78c14eea1fd11d419ecd464de5d
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-16 02:01:12 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
   square = []
-  array.each do |num|
-    square << num ** 2
-  end
-    square
+    array.each do |num|
+      square << num ** 2
+    end
+  square
 end
